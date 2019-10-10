54bf74654db8fdc60795d8ef428a056845b06c99
Sasfer <54417578+Sasfer@users.noreply.github.com>
2019-10-09 22:38:39 -0500

    Cambios finales hilos

--- servidor.py
+++ servidor.py
@@ -1,15 +1,15 @@
+#-*- coding: utf-8-*-
 # Descripcion:
-#   El servidor se encarga de escuchar a través del socket las diversas
+#   El servidor se encarga de escuchar a traves del socket las diversas
 #   peticiones que le hagan los clientes que se conecten a el.
 #
-#	Cada que un cliente desee realizar alguna operación sobre la cuenta
-#	se creará una nueva conexión entre este y el servidor, de tal forma
+#	Cada que un cliente desee realizar alguna operacion sobre la cuenta
+#	se creara una nueva conexión entre este y el servidor, de tal forma
 #	que la conexión que se establece no es persistente.
 
 from operacion import Operacion, generarPassword
 from historial import agregarHistorial
-import socket
-import json
+import socket, json, thread
 
 # Se crea un socket TCP/IP
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,21 +41,24 @@ while True:
         # devuelve el token asignado al cliente que accedio 
         # a la cuenta
         if(p['iniciar'] == True):
-            agregarHistorial(' S ** Cliente' + p.get('cliente') +'inicia sesión ')
+            agregarHistorial(' S ** Cliente ' + p.get('cliente') +' inicia sesion ')
             r = c.abrirTransaccion()
-            agregarHistorial(' S ** Inicia transacción datos ' + r)
+            agregarHistorial(' S ** Inicia transaccion datos ' + r)
             conexion.sendall(str(r).encode())
         # Se ejecuta el método hacer de la instancia de operación se 
         # Se envia: 
         #	- El token asignado al cliente al comenzar a operar sobre la cuenta
+        #	  mas el token que se creo para la operación en ese momento
         #	- La operación que se desea realizar
         #	- Los parametros proporcionados por el cliente dependiendo de la
         #	  operación a realizar sobre la cuente
         # Se regresa:
         # 	- La respuesta de ejecutar la operación indicada sobre la cuenta
         elif(p.get('token')):
-            transaccionToken = generarPassword(10)
+        	# Se crea un token para la operacion
+            transaccionToken = generarPassword(6)
             agregarHistorial(' S ** Hacer -> ')
+            # Se ejecuta la operacion, 
             r = c.operar(p.get('token') + transaccionToken, p.get('operacion'), p.get('parametros'))
             agregarHistorial(' S ** Respuesta ' + str(r))
             conexion.sendall(str(r).encode())
