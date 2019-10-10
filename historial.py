1f665ea54ed8f74ecc6acea2a4359bb78a9de004
Sasfer <54417578+Sasfer@users.noreply.github.com>
2019-10-09 22:35:06 -0500

    Cambios finales hilos

--- historial.py
+++ historial.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8-*-
 # Descripción:
 #	Dado un mensaje, se crear un archivo llamado historial en caso de que no se
 #   haya creado, y se agrega el respectivo mensaje al archivo para tener un 
@@ -6,11 +7,8 @@
 import time
 
 def agregarHistorial(mensaje):
-	
 	historial = open('historial.txt', 'a+')
-	
 	historial.write(' >> ' + str(time.strftime("%d/%m/%y %H:%M:%S - ")) + mensaje + '\n')
-	
 	historial.close()
 
 # Definición para tratar la impresión de los valores de la cuenta
